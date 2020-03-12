711bada56488f12f8abb8e34fce45c09ddff287a
Ben Haus <ben@little-haus.com>
2020-03-12 17:06:42 -0400

    committing my card

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,18 @@
            https://api.github.com/users/<your name>
 */
 
+const entryPoint = document.querySelector('.cards');
+
+axios
+  .get('https://api.github.com/users/phillybenh')
+  .then(response => {
+    console.log(response.data);
+    entryPoint.append(gitCard(response.data));
+  })
+  .catch (error => {
+  console.log("The data was not returned", error);
+});
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -43,10 +55,50 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
+function gitCard(obj) {
+  const newCard = document.createElement('div'),
+    image = document.createElement('img'),
+    infoContainer = document.createElement('div'),
+    name = document.createElement('h3'),
+    userName = document.createElement('p'),
+    location = document.createElement('p'),
+    profile = document.createElement('p'),
+    profileLink = document.createElement('a'),
+    followers = document.createElement('p'),
+    following = document.createElement('p'),
+    bio = document.createElement('p');
 
-/* List of LS Instructors Github username's: 
+//dfdf
+
+  newCard.classList.add('card');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  image.src = obj.avatar_url;
+  image.alt = "Person's GitHub Profile Picture";
+  name.textContent = obj.name;
+  userName.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`;
+  profile.textContent = 'Profile: ';
+  profileLink.textContent = obj.html_url;
+  profileLink.href = obj.html_url;
+  console.log(obj.html_url);
+  console.log(profile);
+  console.log(profileLink);
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = `Bio: ${obj.bio}`;
+
+  newCard.append(image, infoContainer);
+  infoContainer.append(name, userName, location, profile, followers, following, bio);
+  profile.appendChild(profileLink);
+  
+  return newCard;
+};
+
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
